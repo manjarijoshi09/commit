2483dbad9883d4ca1e053b4fb84bda4c779aacf8
Manjari <msnjarij@gmail.com>
2023-03-12 16:54:38 +0000

    hello friend

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("Hello commit");
+        System.out.println("Hello idea");
+        System.out.println("log data");
+        System.out.println("commit")
+
     }
 }
